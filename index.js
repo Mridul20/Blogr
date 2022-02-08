462c701e565512b26a9b529246aff5ccde0d40f9
Mridul20 <mridulmittal20@gmail.com>
2022-02-08 16:59:06 +0530

    bakend initialised

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+app.get("/",function(req,res){
+    res.render("home/index");
+});
+app.listen("3000",function(){
+    console.log("Server started at port 3000");
+});
