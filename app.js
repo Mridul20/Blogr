2f369f2b88b49f99013fd3f726959951ed6372de
Piyush Gurjar <piyushgurjar4064@gmail.com>
2022-02-16 18:08:18 +0530

    DataBase added

--- app.js
+++ app.js
@@ -1,36 +1,55 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const mongoose = require("mongoose");
 
-app.listen("3000",function(){
-    console.log("Server started at port 3000");
+app.listen("3000", function () {
+  console.log("Server started at port 3000");
 });
 
 app.use(express.static("public"));
-app.use(express.json({limit:'1mb'}));
-app.use(bodyParser.urlencoded({extended: true}))
-app.set('view engine', 'ejs');
+app.use(express.json({ limit: "1mb" }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
 
+mongoose.connect("mongodb+srv://admin:admin@cluster0.qyck5.mongodb.net/blogDB");
 
-app.get("/",function(req,res){
-    res.render("home");
+const blogSchema = {
+  id: String,
+  title: String,
+  body: String,
+  tag: Array,
+};
+
+const Blog = mongoose.model("Blog", blogSchema);
+
+app.get("/", function (req, res) {
+  res.render("home");
 });
 
-app.get("/login",function(req,res){
-    res.render("login");
-})
-app.get("/register",function(req,res){
-    res.render("register");
-})
-
-app.get("/edit",function(req,res){
-    res.render("edit");
+app.get("/login", function (req, res) {
+  res.render("login");
+});
+app.get("/register", function (req, res) {
+  res.render("register");
 });
 
-app.post("/saveblogdata",function(req,res){
-    console.log(req.body);
+app.get("/edit", function (req, res) {
+  res.render("edit");
 });
 
-app.post("/search",function(req,res){
-    console.log(req.body);
-})
+app.post("/saveblogdata", function (req, res) {
+  console.log(req.body);
+  console.log(req.body.blogdata);
+  var newblog = new Blog({
+    id: "1234",
+    title: req.body.title,
+    body: String(req.body.blogdata),
+    tag: ["1", "2"],
+  });
+  newblog.save();
+});
+
+app.post("/search", function (req, res) {
+  console.log(req.body);
+});
