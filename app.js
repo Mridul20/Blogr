96eee231dd65b46e5acddc498c094a19612cfbb1
Mridul20 <mridulmittal20@gmail.com>
2022-02-18 00:08:26 +0530

    test python script

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const bcrypt = require('bcryptjs');
 const { use } = require("express/lib/application");
 const url = require('url');
-
+const { spawn } = require("child_process");
 
 app.use(express.static("public"));
 app.use(express.json({ limit: "1mb" }));
@@ -13,6 +13,27 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 mongoose.connect("mongodb+srv://admin:admin@cluster0.yavmq.mongodb.net/blogDB");
+
+
+app.get('/python', (req, res) => {
+ 
+  var dataToSend;
+  // spawn new child process to call the python script
+  const python = spawn('python', ['python/script.py']);
+  // collect data from script
+  python.stdout.on('data', function (data) {
+   console.log('Pipe data from python script ...');
+   dataToSend = data.toString();
+  });
+  // in close event we are sure that stream from child process is closed
+  python.on('close', (code) => {
+  console.log(`child process close all stdio with code ${code}`);
+  // send data to browser
+  console.log(dataToSend);
+  res.send(dataToSend)
+  });
+  
+ }) 
 
 const blogSchema = {
   id: String,
