82b147b68ff7bd595dcb7848c502c5974057e0ce
Parasagrawal <iit2019145@iiita.ac.in>
2022-02-18 13:49:30 +0530

    search functionality added

--- app.js
+++ app.js
@@ -16,24 +16,24 @@ mongoose.connect("mongodb+srv://admin:admin@cluster0.yavmq.mongodb.net/blogDB");
 
 
 app.get('/python', (req, res) => {
- 
+
   var dataToSend;
   // spawn new child process to call the python script
   const python = spawn('python', ['python/script.py']);
   // collect data from script
   python.stdout.on('data', function (data) {
-   console.log('Pipe data from python script ...');
-   dataToSend = data.toString();
+    console.log('Pipe data from python script ...');
+    dataToSend = data.toString();
   });
   // in close event we are sure that stream from child process is closed
   python.on('close', (code) => {
-  console.log(`child process close all stdio with code ${code}`);
-  // send data to browser
-  console.log(dataToSend);
-  res.send(dataToSend)
+    console.log(`child process close all stdio with code ${code}`);
+    // send data to browser
+    console.log(dataToSend);
+    res.send(dataToSend)
   });
-  
- }) 
+
+})
 
 const blogSchema = {
   id: String,
@@ -43,29 +43,29 @@ const blogSchema = {
 };
 
 const userSchema = {
-  username : {
-    type : String,
-    required : true,
+  username: {
+    type: String,
+    required: true,
   },
-  firstName : {
-    type : String,
-    required : true,
+  firstName: {
+    type: String,
+    required: true,
   },
-  lastName : {
-    type : String,
-    required : true,
+  lastName: {
+    type: String,
+    required: true,
   },
-  email : {
-    type : String,
-    required : true,
+  email: {
+    type: String,
+    required: true,
   },
-  password : {
-    type : String,
-    required : true,
+  password: {
+    type: String,
+    required: true,
   },
 }
 const Blog = mongoose.model("Blog", blogSchema);
-const User = mongoose.model("User",userSchema);
+const User = mongoose.model("User", userSchema);
 
 
 
@@ -103,18 +103,9 @@ app.post("/saveblogdata", function (req, res) {
   newblog.save();
 });
 
-app.post("/search", function (req, res) {
-  var tagquery = req.body.search;
 
-  
-  Blog.find({$or:[{tag: tagquery} ,{title : tagquery }]},function(err,result){
-      console.log(result);
-  });
-  res.send("a");
-  // console.log(req.body);
-});
 
-app.post("/register",function(req,res){
+app.post("/register", function (req, res) {
   console.log(req.body);
   var username = req.body.username;
   var firstname = req.body.firstname;
@@ -124,90 +115,118 @@ app.post("/register",function(req,res){
   var password1 = req.body.password1;
   var passwordhash = bcrypt.hashSync(password, salt);
 
-  if(password != password1)
+  if (password != password1)
     return res.redirect(url.format({
-      pathname:"/register",
+      pathname: "/register",
       query: {
         "error": "Passwords do not match",
       }
     }));
 
-  if(!validateEmail(email))
-  return res.redirect(url.format({
-      pathname:"/register",
+  if (!validateEmail(email))
+    return res.redirect(url.format({
+      pathname: "/register",
       query: {
         "error": "Invalid Email",
       }
     }));
 
-  User.find({username : username},function(err,result){
-      if(result.length > 0)
+  User.find({ username: username }, function (err, result) {
+    if (result.length > 0)
       return res.redirect(url.format({
-          pathname:"/register",
-          query: {
-            "error": "Username Exists",
-          }
-        }));      
+        pathname: "/register",
+        query: {
+          "error": "Username Exists",
+        }
+      }));
   });
 
-  User.find({email : email},function(err,result){
-    if(result.length > 0)
-    return res.redirect(url.format({
-        pathname:"/register",
+  User.find({ email: email }, function (err, result) {
+    if (result.length > 0)
+      return res.redirect(url.format({
+        pathname: "/register",
         query: {
           "error": "Email Exists",
         }
-      }));      
-});
+      }));
+  });
   var newUser = new User({
-    username : username,
-    firstName : firstname,
-    lastName : lastname,
-    email : email,
-    password : passwordhash
+    username: username,
+    firstName: firstname,
+    lastName: lastname,
+    email: email,
+    password: passwordhash
   });
   newUser.save();
 
   return res.redirect(url.format({
-    pathname:"/login",
+    pathname: "/login",
     query: {
-      "username" : username   
+      "username": username
     }
-  }));   
+  }));
 });
 
-app.post("/login",function(req,res){
+app.post("/login", function (req, res) {
   console.log(req.body);
   var username = req.body.username;
   var password = req.body.password;
-  
 
-  User.find({username : username},function(err,result){
 
-  
-    if(result.length == 0)
+  User.find({ username: username }, function (err, result) {
+
+
+    if (result.length == 0)
       return res.redirect(url.format({
-        pathname:"/login",
+        pathname: "/login",
         query: {
           "err": "Invalid Username",
         }
       }));
-    if(!bcrypt.compareSync(password, result[0].password))
+    if (!bcrypt.compareSync(password, result[0].password))
       return res.redirect(url.format({
-        pathname:"/login",
+        pathname: "/login",
         query: {
           "err": "Invalid Password",
         }
-      }));   
+      }));
     console.log("hello");
     return res.redirect(url.format({
-      pathname:"/",
+      pathname: "/",
       query: {
-        "login" : true,
-        "username" : username
+        "login": true,
+        "username": username
       }
-    }));  
+    }));
   });
+});
+app.get("/search", function (req, res) {
+  let tagquery = req.query.username;
+  // console.log(tagquery);
+  Blog.find({ $or: [{ tag: tagquery }, { title: tagquery }] }, function (err, result) {
+    console.log(result);
+
+    res.render("search", { result });
+  })
+})
+app.post("/search", function (req, res) {
+  var tagquery = req.body.search;
+  // Blog.find({ $or: [{ tag: tagquery }, { title: tagquery }] }, function (err, result) {
+  //   console.log(result);
+  // var newUser = new User({
+  //   id: result.id,
+  //   title: result.title
+  // });
+  // console.log(newUser);
+
+
+  return res.redirect(url.format({
+    pathname: "/search",
+    query: {
+      // "login": true,
+      "username": tagquery
+    }
+  }));
 });
 
 app.listen("3000", function () {
