9c303a60667ee8eae03da3a9d6b800158c6b68c1
Piyush Gurjar <piyushgurjar4064@gmail.com>
2022-02-23 11:45:19 +0530

    new profile page added

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const url = require('url');
-const fs = require('fs');
+const url = require("url");
+const fs = require("fs");
 const { spawn } = require("child_process");
 const session = require("express-session");
 const passport = require("passport");
 const passporLocalMongoose = require("passport-local-mongoose");
 const { response } = require("express");
-
 
 const app = express();
 
@@ -17,34 +16,38 @@ app.use(express.json({ limit: "1mb" }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
-app.use(session({
-  secret : "Mridul Mittal",
-  resave : false,
-  saveUninitialized : false
-}));
+app.use(
+  session({
+    secret: "Mridul Mittal",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 app.use(passport.initialize());
-app.use(passport.session()); 
+app.use(passport.session());
 
-mongoose.connect("mongodb+srv://admin:admin@cluster0.qyck5.mongodb.net/blogrDB");
+mongoose.connect(
+  "mongodb+srv://admin:admin@cluster0.qyck5.mongodb.net/blogrDB"
+);
 
 const blogSchema = {
   key: String,
   title: String,
   body: String,
   tag: Array,
-  author : String,
-  privacy : Boolean,
+  author: String,
+  privacy: Boolean,
 };
 
 const userSchema = new mongoose.Schema({
   fullname: String,
   email: String,
-  blogs : Array,
+  blogs: Array,
 });
 
 userSchema.plugin(passporLocalMongoose);
-  
+
 const Blog = mongoose.model("Blog", blogSchema);
 const User = mongoose.model("User", userSchema);
 
@@ -52,54 +55,49 @@ passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-
-app.get('/python', (req, res) => {
-
+app.get("/python", (req, res) => {
   var dataToSend;
   // spawn new child process to call the python script
-  const python = spawn('python', ['python/script.py',key]);
+  const python = spawn("python", ["python/script.py", key]);
   // collect data from script
-  python.stdout.on('data', function (data) {
-    console.log('Pipe data from python script ...');
+  python.stdout.on("data", function (data) {
+    console.log("Pipe data from python script ...");
     dataToSend = data.toString();
   });
   // in close event we are sure that stream from child process is closed
-  python.on('close', (code) => {
+  python.on("close", (code) => {
     console.log(`child process close all stdio with code ${code}`);
     // send data to browser
     console.log(dataToSend);
-    res.send(dataToSend)
+    res.send(dataToSend);
   });
+});
 
-})  
-
-app.get('/profile/:username', function(req, res){
+app.get("/profile/:username", function (req, res) {
   console.log(req.params);
-  User.find({username : req.params.username},function(err,result){
-    Blog.find({author:req.params.username},function(error,output){
+  User.find({ username: req.params.username }, function (err, result) {
+    Blog.find({ author: req.params.username }, function (error, output) {
       const data = {
         blog: output,
         name: result[0].fullname,
         username: result[0].username,
-        totblog: result[0].blogs
-      }    
+        totblog: result[0].blogs,
+      };
       return res.render("profile", data);
-    })
-
+    });
 
     const data = {
       name: result[0].fullname,
       username: result[0].username,
-      totblog: result[0].blogs
-    }
+      totblog: result[0].blogs,
+    };
   });
-})
+});
 
 app.get("/", function (req, res) {
-  if(req.isAuthenticated()) 
-    res.render("home",{ login: true,username: req.user.username });
-  else 
-    res.render("home",{ login: false });
+  if (req.isAuthenticated())
+    res.render("home", { login: true, username: req.user.username });
+  else res.render("home", { login: false });
 });
 
 app.get("/login", function (req, res) {
@@ -110,110 +108,123 @@ app.get("/register", function (req, res) {
   res.render("register");
 });
 
-app.get("/logout",function(req,res){
-    req.logout();
-    res.redirect("/");
+app.get("/logout", function (req, res) {
+  req.logout();
+  res.redirect("/");
 });
 
-app.get("/draft/:author/:key",function(req,res){
-  Blog.find({key:req.params.key},function(err,result){
+app.get("/draft/:author/:key", function (req, res) {
+  Blog.find({ key: req.params.key }, function (err, result) {
     console.log(result);
-    if(result.length == 0)
-      return res.render("draft",{author : req.params.author,key : req.params.key,data: null,title : null});
+    if (result.length == 0)
+      return res.render("draft", {
+        author: req.params.author,
+        key: req.params.key,
+        data: null,
+        title: null,
+      });
     else
-     return res.render("draft",{author : req.params.author,key : req.params.key,data : result[0].body,title : result[0].title});
+      return res.render("draft", {
+        author: req.params.author,
+        key: req.params.key,
+        data: result[0].body,
+        title: result[0].title,
+      });
   });
-  
 });
 
 app.get("/edit", function (req, res) {
   const author = req.user.username;
-  User.find({username : author},function(err,result){
-    const key = author  + (result[0].blogs.length + 1);
-    return res.redirect("/draft/" + author  + "/" + key);
+  User.find({ username: author }, function (err, result) {
+    const key = author + (result[0].blogs.length + 1);
+    return res.redirect("/draft/" + author + "/" + key);
   });
 });
 
-app.get("/genaudio/:author/:key",function(req,res){
+app.get("/genaudio/:author/:key", function (req, res) {
   var dataToSend;
-  const python = spawn('python', ['python/txttoaudio.py',req.params.key,req.params.author]);
-  python.stdout.on('data', function (data) {
-    console.log('Pipe data from python script ...');
+  const python = spawn("python", [
+    "python/txttoaudio.py",
+    req.params.key,
+    req.params.author,
+  ]);
+  python.stdout.on("data", function (data) {
+    console.log("Pipe data from python script ...");
     dataToSend = data.toString();
   });
-  python.on('close', (code) => 
-  {
+  python.on("close", (code) => {
     console.log(`child process close all stdio with code ${code}`);
     console.log(dataToSend);
-    res.send(dataToSend)
+    res.send(dataToSend);
   });
 });
 
-app.get("/gentext/:key",function(req,res){
-  Blog.find({key : req.params.key},function(err,result){
-    if(err)
-      console.log(err);
-    if(result.length == 0)
-      res.send("No blog exists");
+app.get("/gentext/:key", function (req, res) {
+  Blog.find({ key: req.params.key }, function (err, result) {
+    if (err) console.log(err);
+    if (result.length == 0) res.send("No blog exists");
     var txtdata = "Title .................. ";
-    const newpar = ".........................................";    
-    txtdata = txtdata + result[0].title + newpar + "written by .................." + result[0].author + newpar;
-    const jsonbody = JSON.parse(result[0].body)
-    for(var i=0;i<jsonbody.blocks.length;i++)
+    const newpar = ".........................................";
+    txtdata =
+      txtdata +
+      result[0].title +
+      newpar +
+      "written by .................." +
+      result[0].author +
+      newpar;
+    const jsonbody = JSON.parse(result[0].body);
+    for (var i = 0; i < jsonbody.blocks.length; i++)
       txtdata = txtdata + jsonbody.blocks[i].data.text + newpar;
     // console.log(txtdata);
-    if (!fs.existsSync("bloggers"))
-      fs.mkdirSync("bloggers");
-    const path = "bloggers/" + result[0].author; 
-    if (!fs.existsSync(path))
-      fs.mkdirSync(path);
-    fs.writeFile(path + "/"  + req.params.key + ".txt",txtdata, function (err) {
-      if (err) 
-        return console.log(err);
+    if (!fs.existsSync("bloggers")) fs.mkdirSync("bloggers");
+    const path = "bloggers/" + result[0].author;
+    if (!fs.existsSync(path)) fs.mkdirSync(path);
+    fs.writeFile(path + "/" + req.params.key + ".txt", txtdata, function (err) {
+      if (err) return console.log(err);
     });
     return res.redirect("/genaudio/" + result[0].author + "/" + req.params.key);
   });
-})
+});
 
 app.post("/saveblogdata", function (req, res) {
   console.log(req.body.blogdata);
-  Blog.find({key : req.body.key},function(err,result){
-      if(err)
-        console.log(err);
-      if(result.length == 0)
-      {
-        console.log("hello");
-        var newblog = new Blog({
-          key: req.body.key,
+  Blog.find({ key: req.body.key }, function (err, result) {
+    if (err) console.log(err);
+    if (result.length == 0) {
+      console.log("hello");
+      var newblog = new Blog({
+        key: req.body.key,
+        title: req.body.title,
+        body: String(req.body.blogdata),
+        tag: ["1", "2"],
+        privacy: false,
+        author: req.body.author,
+      });
+      newblog.save();
+      User.updateMany(
+        { username: req.body.author },
+        { $push: { blogs: req.body.key } },
+        function (erro) {
+          if (erro) console.log(erro);
+        }
+      );
+    } else {
+      console.log(req.body.title);
+      Blog.update(
+        { key: req.body.key },
+        {
           title: req.body.title,
-          body: String(req.body.blogdata),
+          body: req.body.blogdata,
           tag: ["1", "2"],
-          privacy : false,
-          author : req.body.author,
-        });
-        newblog.save();
-        User.updateMany({username : req.body.author},{ $push: { blogs: req.body.key}},function(erro){
-            if(erro)
-              console.log(erro);
-        });
-      }
-      else
-      {
-        console.log(req.body.title);
-        Blog.update({key :req.body.key},{title: req.body.title,body: req.body.blogdata,tag: ["1", "2"],privacy : false,},function(err){
-          if(err)
-            console.log(err);
-        })
-      }
+          privacy: false,
+        },
+        function (err) {
+          if (err) console.log(err);
+        }
+      );
+    }
   });
 });
-
-
-
-
-
-
-
 
 // app.post("/saveblogdata", function (req, res) {
 
@@ -249,60 +260,70 @@ app.post("/saveblogdata", function (req, res) {
 // res.send({author :req.body.author ,key: req.body.key});
 // });
 
-app.post("/register",function(req,res){
+app.post("/register", function (req, res) {
   console.log(req.body);
 
   const usr = new User({
-    username : req.body.username,
-    fullname : req.body.name,
-    email : req.body.email,
-    blogs : [],
-  })
+    username: req.body.username,
+    fullname: req.body.name,
+    email: req.body.email,
+    blogs: [],
+  });
 
-  User.register(usr,req.body.password,function(err,regUser){
-    if(err) {
+  User.register(usr, req.body.password, function (err, regUser) {
+    if (err) {
       console.log(err);
-      return res.redirect(url.format({
-        pathname:"/register",
-        query: {
-          "error": err,
-        }
-      }));
-    }
-    else {
-      passport.authenticate("local")(req,res,function(){
-        req.login(regUser, function(err) {
+      return res.redirect(
+        url.format({
+          pathname: "/register",
+          query: {
+            error: err,
+          },
+        })
+      );
+    } else {
+      passport.authenticate("local")(req, res, function () {
+        req.login(regUser, function (err) {
           if (err) {
             console.log(err);
-            return res.redirect(url.format({
-              pathname:"/register",
-              query: {
-                "error": err,
-              }
-            }))             
+            return res.redirect(
+              url.format({
+                pathname: "/register",
+                query: {
+                  error: err,
+                },
+              })
+            );
           }
-          return res.redirect("/"); 
+          return res.redirect("/");
         });
       });
     }
-  });  
+  });
 });
 
-app.post('/login',passport.authenticate('local', { failureRedirect: '/login' }),function(req, res) {
-    res.redirect('/');
-});
+app.post(
+  "/login",
+  passport.authenticate("local", { failureRedirect: "/login" }),
+  function (req, res) {
+    res.redirect("/");
+  }
+);
 
 app.get("/search/:query", function (req, res) {
   let query = req.params.query;
-  Blog.find({ $or: [{ tag: query }, { title: query }] }, function (err, result) {
-    console.log(result);
-    res.render("search", {result: result});
-  });
+  Blog.find(
+    { $or: [{ tag: query }, { title: query }] },
+    function (err, result) {
+      console.log(result);
+      res.render("search", { result: result });
+    }
+  );
 });
 
 app.post("/search", function (req, res) {
   var query = req.body.search;
-  return res.redirect("/search/" + query)
+  return res.redirect("/search/" + query);
 });
 
 app.listen("3000", function () {
